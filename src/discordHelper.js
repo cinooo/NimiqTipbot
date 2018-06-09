6727b7f8221cae36aaef794c4dad404fbf3fd4d6
Vince <partlycloudy.au@gmail.com>
2018-06-09 21:40:34 +1000

    Add logging

--- src/discordHelper.js
+++ src/discordHelper.js
@@ -103,6 +103,7 @@ e.g. !tip @cino#0628 3`);
       if (sourceAddress === destinationAddress) {
         return reply(`You can't tip to the same wallet address`);
       }
+      console.log(userBalance, nimAmount, parseFloat(userBalance), parseFloat(nimAmount), parseFloat(userBalance) >= parseFloat(nimAmount));
       if (parseFloat(userBalance) >= parseFloat(nimAmount)) {
         await logMessageToHistoryChannel(`Processing !tip from discord: ${discordUserId} for ${nimAmount} NIM`);
         return {
